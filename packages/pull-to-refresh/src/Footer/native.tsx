efc6a436ca27a092491cd23801c2d9e98347d440
alpha <pigeon@shundaojia.com>
2023-04-21 12:28:15 +0800

    处理pull-to-refresh显示状态与实际状态不一致的问题

--- packages/pull-to-refresh/src/Footer/native.tsx
+++ packages/pull-to-refresh/src/Footer/native.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { requireNativeComponent, ViewProps } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { findNodeHandle, requireNativeComponent, UIManager, ViewProps } from 'react-native'
 import { PullToRefreshOffsetChangedEvent, PullToRefreshStateChangedEvent } from '../types'
 
 interface NativePullToRefreshFooterProps extends ViewProps {
@@ -13,11 +13,52 @@ interface NativePullToRefreshFooterProps extends ViewProps {
 
 const NativePullToRefreshFooter = requireNativeComponent<NativePullToRefreshFooterProps>('RefreshFooter')
 
-type NativePullToRefreshFooterInstance = InstanceType<typeof NativePullToRefreshFooter>
+export type NativePullToRefreshFooterInstance = InstanceType<typeof NativePullToRefreshFooter>
+
+const PullToRefreshFooterCommands = {
+  setNativeRefreshing(componentOrHandle: NativePullToRefreshFooterInstance, refreshing: boolean) {
+    UIManager.dispatchViewManagerCommand(findNodeHandle(componentOrHandle), 'setNativeRefreshing', [refreshing])
+  },
+}
 
 const PullToRefreshFooter = React.forwardRef<NativePullToRefreshFooterInstance, NativePullToRefreshFooterProps>(
   (props, ref) => {
-    return <NativePullToRefreshFooter {...props} ref={ref} />
+    const [, forceUpdate] = useState(false)
+    const innerRef = useRef<NativePullToRefreshFooterInstance | null>(null)
+    const nativeRefreshing = useRef(props.refreshing)
+
+    useEffect(() => {
+      nativeRefreshing.current = props.refreshing
+    }, [props.refreshing])
+
+    useEffect(() => {
+      if (props.refreshing !== nativeRefreshing.current && innerRef?.current) {
+        nativeRefreshing.current = props.refreshing
+        PullToRefreshFooterCommands.setNativeRefreshing(innerRef.current, props.refreshing)
+      }
+    })
+
+    const { onRefresh, ..._props } = props
+    const _onRefresh = () => {
+      nativeRefreshing.current = true
+      onRefresh?.()
+      forceUpdate(v => !v)
+    }
+
+    return (
+      <NativePullToRefreshFooter
+        {..._props}
+        ref={instance => {
+          innerRef.current = instance
+          if (typeof ref === 'function') {
+            ref(instance)
+          } else if (ref !== null) {
+            ref.current = instance
+          }
+        }}
+        onRefresh={_onRefresh}
+      />
+    )
   },
 )
 

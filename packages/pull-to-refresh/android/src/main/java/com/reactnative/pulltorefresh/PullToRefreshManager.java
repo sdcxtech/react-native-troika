eb38da567725baa40197a5756b1a425b71bcb0e8
listen <listenzz@163.com>
2023-04-24 11:31:24 +0800

    处理切换 refresh header 的问题

--- packages/pull-to-refresh/android/src/main/java/com/reactnative/pulltorefresh/PullToRefreshManager.java
+++ packages/pull-to-refresh/android/src/main/java/com/reactnative/pulltorefresh/PullToRefreshManager.java
@@ -25,23 +25,34 @@ public class PullToRefreshManager extends ViewGroupManager<PullToRefresh> {
     protected PullToRefresh createViewInstance(@NonNull ThemedReactContext reactContext) {
         PullToRefresh pullToRefresh = new PullToRefresh(reactContext);
         pullToRefresh.setEnableOverScrollBounce(false);
+        pullToRefresh.setEnableOverScrollDrag(true);
+        pullToRefresh.setEnableRefresh(false);
+        pullToRefresh.setEnableLoadMore(false);
         return pullToRefresh;
     }
 
     /**
-     * SmartPullRefreshLayout会对Header、Content、Footer根据需要调整顺序
-     * 因此JSX的所描述的绘制顺序可能与实际的绘制顺序存在差异
-     * 导致removeViewAt无法正确销毁特定子View
-     * 此处记录View原始位置
+     * SmartPullRefreshLayout 会对 Header、Content、Footer 根据需要调整顺序
+     * 因此 JSX 的所描述的绘制顺序可能与实际的绘制顺序存在差异
+     * 导致 removeViewAt 无法正确销毁特定子 View
+     * 此处记录 View 原始位置
      */
     HashMap<Integer, View> reactChildMap = new HashMap<>();
 
     @Override
     public void addView(PullToRefresh parent, View child, int index) {
         if (child instanceof PullToRefreshHeader) {
+            parent.setEnableRefresh(true);
             parent.setRefreshHeader((RefreshHeader) child);
+            if (parent.isLaidOut()) {
+                ((PullToRefreshHeader) child).onInitialized(parent.getRefreshKernel(), 0, 0);
+            }
         } else if (child instanceof PullToRefreshFooter) {
+            parent.setEnableLoadMore(true);
             parent.setRefreshFooter((RefreshFooter) child);
+            if (parent.isLaidOut()) {
+                ((PullToRefreshFooter) child).onInitialized(parent.getRefreshKernel(), 0, 0);
+            }
         } else {
             parent.setRefreshContent(child);
         }
@@ -50,9 +61,16 @@ public class PullToRefreshManager extends ViewGroupManager<PullToRefresh> {
 
     @Override
     public void removeViewAt(PullToRefresh parent, int index) {
-        View view = reactChildMap.get(index);
+        View child = reactChildMap.get(index);
         for (int i = 0; i < parent.getChildCount(); i++) {
-            if (view == parent.getChildAt(i)) {
+            if (child == parent.getChildAt(i)) {
+                if (child instanceof PullToRefreshHeader) {
+                    parent.setEnableRefresh(false);
+                    parent.setOnRefreshListener(null);
+                } else if (child instanceof PullToRefreshFooter) {
+                    parent.setEnableLoadMore(false);
+                    parent.setOnLoadMoreListener(null);
+                }
                 super.removeViewAt(parent, i);
                 break;
             }

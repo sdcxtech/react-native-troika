548fe5df4f59811d9a016c1a53ce1a2900ef1e02
listen <listenzz@163.com>
2023-05-25 15:05:33 +0800

    [activity-indicator] 添加 activity-indicator package

---
+++ packages/activity-indicator/README.md
@@ -0,0 +1,16 @@
+# ActivityIndicator
+
+`ActivityIndicator` 是一个 React Native 原生 UI 组件，是一个菊花转圈的动画。
+
+## Installation
+
+```bash
+yarn install @sdcx/activity-indicator
+```
+
+## Usage
+
+```tsx
+import ActivityIndicator from '@sdcx/activity-indicator'
+// API 什么的和 官方的 ActivityIndicator 完全一样
+```
---
+++ packages/activity-indicator/android/src/main/java/com/reactnative/activityindicator/ActivityIndicatorModule.java
@@ -0,0 +1,30 @@
+package com.reactnative.activityindicator;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class ActivityIndicatorModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public ActivityIndicatorModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "RNActivityIndicator";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        // TODO: Implement some actually useful functionality
+        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+}

aeec9edd7e8da4e0865fda63d023931c0c4dbe28
listen <listenzz@163.com>
2023-05-29 11:25:30 +0800

    [activity-indicator] 略微优化

--- demo/activity-indicator/ActivityIndicatorScreen.tsx
+++ demo/activity-indicator/ActivityIndicatorScreen.tsx
@@ -9,7 +9,7 @@ function ActivityIndicatorScreen() {
       <ActivityIndicator />
       <ActivityIndicator size="large" />
       <ActivityIndicator size="small" color="#0000ff" />
-      <ActivityIndicator size="large" color="#00ff00" animating={false} hidesWhenStopped={false} />
+      <ActivityIndicator size="large" color="#00ff00" animating={false} />
     </View>
   )
 }

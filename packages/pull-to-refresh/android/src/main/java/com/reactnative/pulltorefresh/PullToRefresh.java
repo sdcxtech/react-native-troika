cc0a43b08d7802ae7a924e904db8d66846775cc0
listen <listenzz@163.com>
2023-09-13 18:36:40 +0800

    [pull-to-refresh] 处理数据不足以填满整个页面时嵌套滚动不生效的问题
    fix #12

--- packages/pull-to-refresh/android/src/main/java/com/reactnative/pulltorefresh/PullToRefresh.java
+++ packages/pull-to-refresh/android/src/main/java/com/reactnative/pulltorefresh/PullToRefresh.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.core.view.ViewCompat;
 
 import com.facebook.react.uimanager.events.NativeGestureUtil;
 import com.scwang.smart.refresh.layout.SmartRefreshLayout;
@@ -23,8 +27,8 @@ public class PullToRefresh extends SmartRefreshLayout {
 
     private final Runnable measureAndLayout = () -> {
         measure(
-                View.MeasureSpec.makeMeasureSpec(getWidth(), View.MeasureSpec.EXACTLY),
-                View.MeasureSpec.makeMeasureSpec(getHeight(), View.MeasureSpec.EXACTLY));
+            View.MeasureSpec.makeMeasureSpec(getWidth(), View.MeasureSpec.EXACTLY),
+            View.MeasureSpec.makeMeasureSpec(getHeight(), View.MeasureSpec.EXACTLY));
         layout(getLeft(), getTop(), getRight(), getBottom());
     };
 
@@ -32,6 +36,38 @@ public class PullToRefresh extends SmartRefreshLayout {
     public void requestLayout() {
         super.requestLayout();
         post(measureAndLayout);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        ViewGroup view = (ViewGroup) mRefreshContent.getScrollableView();
+        // 数据不足以填满整个页面
+        if (!view.canScrollVertically(-1) && !view.canScrollVertically(1)) {
+            final ViewParent parent = getParent();
+            if (parent != null) {
+                parent.requestDisallowInterceptTouchEvent(true);
+            }
+            
+            final float offsetX = getScrollX() - view.getLeft();
+            final float offsetY = getScrollY() - view.getTop();
+            final MotionEvent transformedEvent = MotionEvent.obtain(ev);
+            transformedEvent.offsetLocation(offsetX, offsetY);
+            view.onInterceptTouchEvent(transformedEvent);
+            view.onTouchEvent(transformedEvent);
+            transformedEvent.recycle();
+            
+            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+                view.startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL);
+            }
+            
+            if (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_CANCEL) {
+                view.stopNestedScroll();
+            }
+            
+            return true;
+        }
+
+        return super.dispatchTouchEvent(ev);
     }
 
     public boolean onInterceptTouchEvent(MotionEvent ev) {

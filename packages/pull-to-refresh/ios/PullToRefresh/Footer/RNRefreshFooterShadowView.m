017ffa71934a05e679445fecae2fda87c215e26b
listen <listenzz@163.com>
2025-02-28 14:50:40 +0800

    [pull-to-refresh] Fix #47

--- packages/pull-to-refresh/ios/PullToRefresh/Footer/RNRefreshFooterShadowView.m
+++ packages/pull-to-refresh/ios/PullToRefresh/Footer/RNRefreshFooterShadowView.m
@@ -3,6 +3,17 @@
 
 @implementation RNRefreshFooterShadowView
 
+- (instancetype)init {
+    if (self = [super init]) {
+        self.top = (YGValue){1000, YGUnitPoint};
+        self.bottom = YGValueUndefined;
+        self.left = YGValueZero;
+        self.right = YGValueZero;
+        self.position = YGPositionTypeAbsolute;
+    }
+    return self;
+}
+
 - (void)setLocalData:(RNRefreshFooterLocalData *)localData {
     CGSize size = localData.scrollViewContentSize;
     self.top = (YGValue){size.height, YGUnitPoint};
--- packages/pull-to-refresh/ios/PullToRefresh/Header/RNRefreshHeaderShadowView.m
+++ packages/pull-to-refresh/ios/PullToRefresh/Header/RNRefreshHeaderShadowView.m
@@ -5,6 +5,17 @@
 
 @implementation RNRefreshHeaderShadowView
 
+- (instancetype)init {
+    if (self = [super init]) {
+        self.top = (YGValue){-1000, YGUnitPoint};
+        self.bottom = YGValueUndefined;
+        self.left = YGValueZero;
+        self.right = YGValueZero;
+        self.position = YGPositionTypeAbsolute;
+    }
+    return self;
+}
+
 - (void)setLocalData:(RNRefreshHeaderLocalData *)localData {
     self.top = (YGValue){-localData.height, YGUnitPoint};
     self.bottom = YGValueUndefined;

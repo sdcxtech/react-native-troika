5ff8c7f4037e8604d9e1ce7cb343291270d2e4d2
listen <listenzz@163.com>
2025-05-15 10:32:00 +0800

    升级到 RN0.76

--- android/app/src/main/java/com/example/myuidemo/MainApplication.java
+++ android/app/src/main/java/com/example/myuidemo/MainApplication.java
@@ -1,52 +1,78 @@
 package com.example.myuidemo;
 
 import android.app.Application;
-
 import com.facebook.common.logging.FLog;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactHost;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
 import com.reactnative.hybridnavigation.ReactBridgeManager;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-	private final ReactNativeHost mReactNativeHost =
-		new ReactNativeHost(this) {
-			@Override
-			public boolean getUseDeveloperSupport() {
-				return BuildConfig.DEBUG;
-			}
+    private final ReactNativeHost reactNativeHost = new DefaultReactNativeHost(this) {
+        @Override
+        public List<ReactPackage> getPackages() {
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return packages;
+        }
 
-			@Override
-			protected List<ReactPackage> getPackages() {
-				@SuppressWarnings("UnnecessaryLocalVariable")
-				List<ReactPackage> packages = new PackageList(this).getPackages();
-				packages.add(new MyUiPackage());
-				return packages;
-			}
+        @Override
+        public String getJSMainModuleName() {
+            return "index";
+        }
 
-			@Override
-			protected String getJSMainModuleName() {
-				return "index";
-			}
-		};
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
-	@Override
-	public ReactNativeHost getReactNativeHost() {
-		return mReactNativeHost;
-	}
+        @Override
+        public boolean isNewArchEnabled() {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+        
+        @Override
+        public Boolean isHermesEnabled() {
+            return BuildConfig.IS_HERMES_ENABLED;
+        }
+    };
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		SoLoader.init(this, /* native exopackage */ false);
-		ReactBridgeManager bridgeManager = ReactBridgeManager.get();
-		bridgeManager.install(getReactNativeHost());
-		FLog.setMinimumLoggingLevel(FLog.INFO);
-	}
-	
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return reactNativeHost;
+    }
+
+    @Override
+    public ReactHost getReactHost() {
+        return DefaultReactHost.getDefaultReactHost(getApplicationContext(), reactNativeHost);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            DefaultNewArchitectureEntryPoint.load();
+        }
+        ReactBridgeManager bridgeManager = ReactBridgeManager.get();
+        bridgeManager.install(reactNativeHost);
+        FLog.setMinimumLoggingLevel(FLog.INFO);
+    }
 }

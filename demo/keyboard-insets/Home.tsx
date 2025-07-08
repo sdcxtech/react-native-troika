f71db271eda9f1660622cc7c1033ed896ffc54ca
listen <listenzz@163.com>
2025-07-08 12:25:21 +0800

    [keyboard-insets] 更新 demo

--- demo/keyboard-insets/Home.tsx
+++ demo/keyboard-insets/Home.tsx
@@ -1,87 +1,100 @@
 import React from 'react';
-import {StyleSheet, FlatList, ListRenderItem, Text, TouchableOpacity, Image} from 'react-native';
+import {
+    StyleSheet,
+    FlatList,
+    ListRenderItem,
+    Text,
+    TouchableOpacity,
+    Image,
+} from 'react-native';
 import {useNavigator, withNavigationItem} from 'hybrid-navigation';
 
 interface Item {
-  title: string;
-  routeName: string;
-  action?: string;
+    title: string;
+    routeName: string;
+    action?: string;
 }
 
 const data: Array<Item> = [
-  {
-    title: 'Keyboard Advoiding',
-    routeName: 'KeyboardAdvoiding',
-  },
-  {
-    title: '聊天键盘处理',
-    routeName: 'KeyboardChat',
-  },
-  {
-    title: '聊天键盘处理(Reanimated)',
-    routeName: 'KeyboardChatReanimated',
-  },
-  {
-    title: 'Modal + TextInput',
-    routeName: 'ModalTextInput',
-    action: 'modal',
-  },
+    {
+        title: 'Keyboard Avoiding',
+        routeName: 'KeyboardAvoiding',
+    },
+    {
+        title: '聊天键盘处理',
+        routeName: 'KeyboardChat',
+    },
+    {
+        title: '聊天键盘处理(Reanimated)',
+        routeName: 'KeyboardChatReanimated',
+    },
+    {
+        title: 'Modal + TextInput',
+        routeName: 'ModalTextInput',
+        action: 'modal',
+    },
 ];
 
 function Home() {
-  const navigator = useNavigator();
+    const navigator = useNavigator();
 
-  const renderListItem: ListRenderItem<Item> = ({item}) => {
+    const renderListItem: ListRenderItem<Item> = ({item}) => {
+        return (
+            <ListItem
+                {...item}
+                onPress={() => {
+                    if (item.action === 'modal') {
+                        navigator.showModal(item.routeName);
+                    } else {
+                        navigator.push(item.routeName);
+                    }
+                }}
+            />
+        );
+    };
+
     return (
-      <ListItem
-        {...item}
-        onPress={() => {
-          if (item.action === 'modal') {
-            navigator.showModal(item.routeName);
-          } else {
-            navigator.push(item.routeName);
-          }
-        }}
-      />
+        <FlatList
+            data={data}
+            keyExtractor={item => item.title}
+            renderItem={renderListItem}
+        />
     );
-  };
-
-  return <FlatList data={data} keyExtractor={item => item.title} renderItem={renderListItem} />;
 }
 
 interface ListItemProps {
-  title: string;
-  onPress?: () => void;
+    title: string;
+    onPress?: () => void;
 }
 
 function ListItem({title, onPress}: ListItemProps) {
-  return (
-    <TouchableOpacity style={styles.item} onPress={onPress}>
-      <Text style={styles.text}>{title}</Text>
-      <Image source={require('assets/indicator.png')} />
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity style={styles.item} onPress={onPress}>
+            <Text style={styles.text}>{title}</Text>
+            <Image source={require('assets/indicator.png')} />
+        </TouchableOpacity>
+    );
 }
 
 export default withNavigationItem({
-  titleItem: {
-    title: 'Keyboard',
-  },
+    titleItem: {
+        title: 'Keyboard',
+    },
 })(Home);
 
 const styles = StyleSheet.create({
-  item: {
-    height: 60,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#EEEEEE',
-    paddingLeft: 16,
-    paddingRight: 16,
-  },
-  text: {
-    color: '#222222',
-    fontSize: 17,
-  },
+    item: {
+        height: 60,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#EEEEEE',
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    text: {
+        color: '#222222',
+        fontSize: 17,
+    },
 });
--- demo/keyboard-insets/index.ts
+++ demo/keyboard-insets/index.ts
@@ -1,15 +1,18 @@
 import Navigation from 'hybrid-navigation';
 
 import Home from './Home';
-import KeyboardAdvoiding from './KeyboardAdvoiding';
+import KeyboardAvoiding from './KeyboardAvoiding';
 import KeyboardChat from './KeyboardChat';
 import KeyboardChatReanimated from './KeyboardChatReanimated';
 import ModalTextInput from './ModalTextInput';
 
 export function registerKeyboardComponent() {
-  Navigation.registerComponent('Keyboard', () => Home);
-  Navigation.registerComponent('KeyboardAdvoiding', () => KeyboardAdvoiding);
-  Navigation.registerComponent('KeyboardChat', () => KeyboardChat);
-  Navigation.registerComponent('KeyboardChatReanimated', () => KeyboardChatReanimated);
-  Navigation.registerComponent('ModalTextInput', () => ModalTextInput);
+    Navigation.registerComponent('Keyboard', () => Home);
+    Navigation.registerComponent('KeyboardAvoiding', () => KeyboardAvoiding);
+    Navigation.registerComponent('KeyboardChat', () => KeyboardChat);
+    Navigation.registerComponent(
+        'KeyboardChatReanimated',
+        () => KeyboardChatReanimated,
+    );
+    Navigation.registerComponent('ModalTextInput', () => ModalTextInput);
 }

22d9d7aacd59e93a8088e862a6440515f10eb0e2
listen <listenzz@163.com>
2025-12-07 23:29:44 +0800

    升级到 RN0.82

--- android/app/src/main/java/com/example/myuidemo/MainApplication.java
+++ android/app/src/main/java/com/example/myuidemo/MainApplication.java
@@ -15,7 +15,8 @@ import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
-import com.reactnative.hybridnavigation.ReactBridgeManager;
+import com.reactnative.hybridnavigation.ReactManager;
+
 
 import java.io.IOException;
 import java.util.List;
@@ -23,7 +24,8 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost reactNativeHost = new DefaultReactNativeHost(this) {
-        @Override
+		@NonNull
+		@Override
         public List<ReactPackage> getPackages() {
             List<ReactPackage> packages = new PackageList(this).getPackages();
             // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -31,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
             return packages;
         }
 
+		@NonNull
         @Override
         public String getJSMainModuleName() {
             return "index";
@@ -47,36 +50,33 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        public Boolean isHermesEnabled() {
+        public boolean isHermesEnabled() {
             return BuildConfig.IS_HERMES_ENABLED;
         }
     };
 
-    @NonNull
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return reactNativeHost;
-    }
-
-    @Override
-    public ReactHost getReactHost() {
-        return DefaultReactHost.getDefaultReactHost(getApplicationContext(), reactNativeHost, null);
-    }
+	@NonNull
+	@Override
+	public ReactHost getReactHost() {
+		return DefaultReactHost.getDefaultReactHost(getApplicationContext(), reactNativeHost, null);
+	}
 
     @Override
     public void onCreate() {
         super.onCreate();
-        try {
-            SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            DefaultNewArchitectureEntryPoint.load();
-        }
-        ReactBridgeManager bridgeManager = ReactBridgeManager.get();
-        bridgeManager.install(reactNativeHost);
+		try {
+			SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+			// If you opted-in for the New Architecture, we load the native entry point for this app.
+			DefaultNewArchitectureEntryPoint.load();
+		}
+
+		ReactManager reactManager = ReactManager.get();
+		reactManager.install(getReactHost());
+
         FLog.setMinimumLoggingLevel(FLog.INFO);
     }
 }
